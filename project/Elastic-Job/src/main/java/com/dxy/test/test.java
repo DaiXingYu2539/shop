a5924c926257079d7751a47d18533e70f06f3cb1
DaiXingYu2539 <1144673779@qq.com>
2021-10-20 13:34:12 +0800

    第san次提交

---
+++ project/Elastic-Job/src/main/java/com/dxy/test/test.java
@@ -0,0 +1,13 @@
+package com.dxy.test;
+
+import java.util.Random;
+
+public class test {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        System.out.println(random.nextInt(100));
+
+    }
+}
